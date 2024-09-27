3d32152fc6da6ec833e7f092b5e036a620b36393
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-09-28 00:50:56 +0100

    fix(redis): fixed set with duration function.

---
+++ main.js
@@ -0,0 +1,12 @@
+const redisClient = require('./utils/redis.js');
+
+(async () => {
+  console.log('1> ', redisClient.isAlive());
+  console.log('2> ', await redisClient.get('myKey'));
+  await redisClient.set('myKey', 12, 5);
+  console.log('3> ', await redisClient.get('myKey'));
+
+  setTimeout(async () => {
+    console.log('4> ', await redisClient.get('myKey'));
+  }, 1000 * 10);
+})();
